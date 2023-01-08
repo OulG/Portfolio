961031198d08b2cc90c3324243145e1461d757b0
OulG <lou.gain@outlook.fr>
2023-01-08 12:04:59 +0100

    Add multer

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,10 +4,12 @@ const adminController = require("../controllers/adminController");
 
 const router = express.Router();
 
+const multer = require("../middlewares/multer");
+
 router.get("/", adminController.getAll);
 router.get("/:id", adminController.getOne);
-router.post("/", adminController.addOne);
-router.put("/:id", adminController.updateOne);
+router.post("/", multer, adminController.addOne);
+router.put("/:id", multer, adminController.updateOne);
 router.delete("/:id", adminController.deleteOne);
 
 module.exports = router;
