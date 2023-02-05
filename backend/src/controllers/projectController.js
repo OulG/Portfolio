9a7a4005b6abc1f98c7031782487fed8389c860e
OulG <lou.gain@outlook.fr>
2023-02-05 22:17:06 +0100

    Add function to centralize findone method in projectRoutes

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -4,7 +4,6 @@ const projectToolDataAccess = require("../models/projectToolDataAccess");
 exports.getAll = async (req, res, next) => {
   try {
     const projects = await projectDataAccess.findAll();
-
     res.send(projects);
   } catch (error) {
     next(error);
@@ -13,37 +12,32 @@ exports.getAll = async (req, res, next) => {
 
 exports.getOne = async (req, res, next) => {
   try {
-    const projectId = parseInt(req.params.id, 10);
-
-    const project = await projectDataAccess.findOne(projectId);
-
-    if (project.length === 0) {
-      res.sendStatus(404);
-    } else {
-      const projectTools = await projectToolDataAccess.findByProject(projectId);
-      project.tools = projectTools;
-      res.send(project);
-    }
+    const { project } = req;
+    const projectTools = await projectToolDataAccess.findByProject(project.id);
+    project.tools = projectTools;
+    res.send(project);
   } catch (error) {
     next(error);
   }
 };
 
 exports.addOne = async (req, res, next) => {
-  const { title, description, picture, link, projectTools } = req.body;
+  const { title, description, link, projectTools } = req.body;
 
   try {
     const newProject = await projectDataAccess.create({
       title,
       description,
-      picture,
+      picture: `assets/images/${req.file.filename}`,
       link,
     });
-    // Pour chaque tool, je fais une requête qui vient lié l'Id du project & l'Id du tool.
-    const request = projectTools.map((toolId) => {
-      return projectToolDataAccess.create(newProject.id, toolId);
-    });
-    await Promise.all(request);
+    if (projectTools) {
+      // Pour chaque tool, je fais une requête qui vient lié l'Id du project & l'Id du tool.
+      const request = projectTools.map((toolId) => {
+        return projectToolDataAccess.create(newProject.id, toolId);
+      });
+      await Promise.all(request);
+    }
     res.sendStatus(201);
   } catch (error) {
     next(error);
@@ -75,6 +69,7 @@ exports.deleteOne = async (req, res, next) => {
   const projectId = parseInt(req.params.id, 10);
   try {
     await projectDataAccess.destroy(projectId);
+    res.sendStatus(201);
   } catch (error) {
     next(error);
   }
--- backend/src/routes/projectRoutes.js
+++ backend/src/routes/projectRoutes.js
@@ -1,13 +1,31 @@
 const express = require("express");
-
+const projectDataAccess = require("../models/projectDataAccess");
 const projectController = require("../controllers/projectController");
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer");
 
 const router = express.Router();
+router.use("/:id", async (req, res, next) => {
+  try {
+    const projectId = parseInt(req.params.id, 10);
+
+    const project = await projectDataAccess.findOne(projectId);
+
+    if (!project) {
+      res.status(404).send("Project not found");
+    } else {
+      req.project = project;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 router.get("/", projectController.getAll);
 router.get("/:id", projectController.getOne);
-router.post("/", projectController.addOne);
-router.put("/:id", projectController.updateOne);
-router.delete("/:id", projectController.deleteOne);
+router.post("/", auth, multer, projectController.addOne);
+router.put("/:id", auth, projectController.updateOne);
+router.delete("/:id", auth, projectController.deleteOne);
 
 module.exports = router;
